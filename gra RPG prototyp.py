cad8426d78c1aa3d2c9acce8dc2671d474c8328f
dawid2010wieczorek-dot <dawid2010wieczorek@gmail.com>
2025-10-30 15:12:58 +0100

    Refactor RPG game logic and add new characters

--- gra RPG prototyp.py
+++ gra RPG prototyp.py
@@ -1,17 +1,20 @@
-postac = [300,10]
+postac = [1000,10]
 goblin = [70,10]
 straznik = [270,90]
+czarodziej = [200,9999,99999]
+smok = [1000,100]
+krol = [10,10,99999]
+
 
 while postac[0] >= 0:
     print("walka - a")
     inp = input()
     if inp == "a":
-        while goblin[0] >= 0:
-            while postac[0] >= 0:
-                print(postac)
-                print(goblin)
-                postac[0] -= goblin[1]
-                goblin[0] -= postac[1]
+        while postac[0] > 0:
+            print(postac)
+            print(goblin)
+            postac[0] -= goblin[1]
+            goblin[0] -= postac[1]
             if goblin[0] <= 0:
                 print("zwycięstwo")
                 print("level up")
@@ -19,29 +22,82 @@ while postac[0] >= 0:
                 print (postac)
                 print("dotarłeś do sklepu")
                 print("atak czy obrona")
-                print("atak to + 30 do ataku")
+                print("atak to + 30 do ataku i lekki boost do hp")
                 print("obrona to + 50 do obrony")
                 wybór = input()
                 if wybór == "atak":
-                    postac = [300,90]
-                if wybór == "obrona":
+                    postac = [1000,90]
+                elif wybór == "obrona":
                     postac = [350,60]
                 else:
                     print("wybierz coś istotnego")
                 print(postac)
-                while straznik[0] >= 0:
-                    while postac[0] >= 0:
-                        print(postac)
-                        print(straznik)
-                        postac[0] -= straznik[1]
-                        straznik[0] -= postac[1]
+                while postac[0] > 0:
+                    print(postac)
+                    print(straznik)
+                    straznik[0] -= postac[1]
+                    postac[0] -= straznik[1]
                     if straznik[0] <= 0:
                         print("zwycięstwo")
-                        print("ciąg dalszy nastąpi")
-                    else:
-                        print("game over")
-            else:
-                print("game over")            
-                
+                        break
+                if straznik[0] <= 0:
+                    print("napotykasz się na tajmeniczego czarodzieja")
+                    print("co robisz ?")
+                    print("rozmawiam,proszę o pomoc,atakuję")
+                    rozmowa = input()
+                    if rozmowa == "rozmawiam":
+                        postac[0] += czarodziej[0]
+                        print(postac)
+                    elif rozmowa =="proszę o pomoc":
+                        postac[1] += czarodziej[1]
+                        print(postac)
+                    elif rozmowa =="atakuje":
+                        postac[0] -= czarodziej[2]
+                        if postac[0] <= 0:
+                            print("game over")
+                            break  
+                    print("teraz widzisz przed sobą ogramnego smoka")
+                    print("co robisz")
+                    print("uciekam,atakuje,rozmawiam")
+                    smoczek = input()
+                    if smoczek == "uciekam":
+                        print("nie ma ucieczki")
+                        postac[1] -= krol[2]
+                    elif smoczek == "rozmawiam":
+                        print("w tym świecie amoki nie rozmawiają")
+                        postac[1] -= krol[2]
+                    elif smoczek == "atakuje":
+                        while postac[0] > 0:
+                            print(postac)
+                            print(smok)
+                            smok[0] -= postac[1]
+                            postac[0] -= smok[1]
+                            if smok[0] <= 0:
+                                print("zwycięstwo")
+                                break
+                        if smok[0] <= 0:
+                            print("napotykasz się na bardzo złego krola")
+                            print("ktory torturuje swoich poddanych")
+                            print("i zostałeś wezwany by go unicestwić")
+                            print("co zamierzasz uczynić")
+                            print("zabijam go,próbuje go przekonać aby się zmienił,uciekam")
+                            krolewicz = input()
+                            if krolewicz == "uciekam":
+                                print("nie ma ucieczki")
+                                postac[1] -= krol[2]
+                            elif krolewicz == "próbuje go przekonać aby się zmienił":
+                                print("nie mam takiego zamiaru")
+                                print("ale moge cie zabić")
+                                postac[1] -= krol[2]
+                            elif krolewicz == "zabijam go":
+                                krol[0] -= postac[1]
+                                print(krol)
+                                print(postac)
+                            if krol[0] <= 0:
+                                print("udało ci sie zgładzić złego krola")
+                                print("gratulacje za przejście mojej malej gry")
+                                print("yipee")
 
-    
+
+        if postac[0] <= 0:
+                print("game over")  
